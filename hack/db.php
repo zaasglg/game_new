7577c32287a4a04b355f8dd380f662015f87d2c0
zaasglg <n4msin@mail.ru>
2025-09-06 01:54:50 +0500

    wewfe

--- hack/db.php
+++ hack/db.php
@@ -3,7 +3,7 @@ $host = 'localhost';
 $dbname = 'volurgame';
 $username = 'p@ssword';
 $password = 'root';
-$port = 8889;
+$port = 3306;
 $socket = '/var/run/mysqld/mysqld.sock'; // Уточните путь на сервере
 
 try {
