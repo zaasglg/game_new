8381f76aa429ff7ae2c56f3e6571cacd247185e1
zaasglg <n4msin@mail.ru>
2025-09-07 16:54:13 +0500

    reerg

--- hack/chicken_index.php
+++ hack/chicken_index.php
@@ -11,25 +11,43 @@
     <link rel="stylesheet" href="./css/style.css?v=1.0">
     <link rel="icon" href="./images/authorization.png" />
 
-    <!-- jQuery (должен быть первым) -->
+    <!-- jQuery (обязательно первым) -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <!-- Правильный notify.js -->
+    <!-- notify.js (старая версия с $.notify) -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js"></script>
 </head>
 <body style="background: #000;">
 
     <div class="main__wrapper">
+        <!-- Картинки (как в оригинале) -->
+        <img class="money__top--left" src="./images/money_top_left.webp" alt="money">
+        <img class="money__top--right" src="./images/money_top_right.webp" alt="money">
+        <img class="money__left--center" src="./images/money_left_center.webp" alt="money">
+        <img class="money__right--center" src="./images/money_right_center.webp" alt="money">
+        
         <div class="main">
             <h1 class="translate" data-key="welcome">🐔 ¡Bienvenido al Chicken Road Hack Bot!</h1>   
 
+            <!-- Форма -->
             <form id="chickenLoginForm" style="display: flex; flex-direction: column; align-items: center; width: 100%;">
-                <input class="translate-placeholder" data-key="input_id" style="margin-bottom: 10px;" type="text" name="user_id" id="user_id" placeholder="Introduce tu ID de usuario" required>
-                <button style="background: #FFD900" class="btn translate" type="submit" data-key="sign_in">Acceder al Hack Bot</button>
+                <input class="translate-placeholder" 
+                       data-key="input_id" 
+                       style="margin-bottom: 10px;" 
+                       type="text" 
+                       name="user_id" 
+                       id="user_id" 
+                       placeholder="Introduce tu ID de usuario" 
+                       required>
+                <button style="background: #FFD900" 
+                        class="btn translate" 
+                        type="submit" 
+                        data-key="sign_in">Acceder al Hack Bot</button>
             </form>
             
             <p id="errorMessage" style="color: red; display: none;"></p>
             
+            <!-- Скрипт -->
             <script>
                 $(document).ready(function() {
                     console.log("jQuery version:", $.fn.jquery);
@@ -40,8 +58,9 @@
 
                         let user_id = $("#user_id").val();
 
+                        // Проверка заполненности
                         if (!user_id || isNaN(user_id)) {
-                            $.notify("Por favor, introduce un ID de usuario válido", "error");
+                            $.notify("Fill in the account ID", "error"); // сообщение на английском
                             return;
                         }
 
@@ -70,10 +89,18 @@
                     });
                 });
             </script>
+
+            <!-- Переключатель языка -->
+            <label class="switch">
+                <p class="es">ES</p>
+                <input type="checkbox" class="toggle">
+                <span class="slider round"></span>
+                <p class="eng">ENG</p>
+            </label>
         </div>
     </div>
 
-    <!-- Остальные скрипты -->
+    <!-- Скрипты -->
     <script src="./js/toggle.js?v=1.0"></script>
     <script src="./js/script.js?v=1.0"></script>
     <script src="./js/lang.js?v=1.0"></script>
