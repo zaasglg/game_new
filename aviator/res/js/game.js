63c1b70f87bbe1f11edde7951641992e62e868f7
zaasglg <n4msin@mail.ru>
2025-09-08 06:51:26 +0500

    fwewef

--- aviator/res/js/game.js
+++ aviator/res/js/game.js
@@ -1105,7 +1105,7 @@ setTimeout(function() {
     }
 }, 3000);
 
-var socket = io.connect('https://valor-games.com:2345');    
+var socket = io.connect('wss://valor-games.com/socket.io');    
 socket.on('message', ( msg ) => { 
     console.log('New message: ', msg ); 
     var $obj = typeof msg == "string" ? eval('('+ msg +')') : msg; 
