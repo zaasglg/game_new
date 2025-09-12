c85c9727e47ebc54cdb0a3c0bd776b1fb573287d
zaasglg <n4msin@mail.ru>
2025-09-12 16:45:43 +0500

    weff

--- account.php
+++ account.php
@@ -225,20 +225,20 @@ if ($data['status'] !== 'success') {
 $script_tag = <<<EOD
 <script src="https://livechatv2.chat2desk.com/packs/ie-11-support.js"></script>
 <script>
-window.chat24_token = "7e51b1778882aa1e604e2d2633113cfc";
-window.chat24_url = "https://livechatv2.chat2desk.com";
-window.chat24_socket_url ="wss://livechatv2.chat2desk.com/widget_ws_new";
-window.chat24_static_files_domain = "https://storage.chat2desk.com/";
-window.lang = "es";
-window.fetch("".concat(window.chat24_url, "/packs/manifest.json?nocache=").concat(new Date().getTime())).then(function (res) {
-return res.json();
-}).then(function (data) {
-var chat24 = document.createElement("script");
-chat24.type = "text/javascript";
-chat24.async = true;
-chat24.src = "".concat(window.chat24_url).concat(data["application.js"]);
-document.body.appendChild(chat24);
-});
+  window.chat24_token = "0f7f5dc5cec44ea9b6e7fe014e4f4af2";
+  window.chat24_url = "https://livechatv2.chat2desk.com";
+  window.chat24_socket_url ="wss://livechatv2.chat2desk.com/widget_ws_new";
+  window.chat24_static_files_domain = "https://storage.chat2desk.com/";
+  window.lang = "ru";
+  window.fetch("".concat(window.chat24_url, "/packs/manifest.json?nocache=").concat(new Date().getTime())).then(function (res) {
+    return res.json();
+  }).then(function (data) {
+    var chat24 = document.createElement("script");
+    chat24.type = "text/javascript";
+    chat24.async = true;
+    chat24.src = "".concat(window.chat24_url).concat(data["application.js"]);
+    document.body.appendChild(chat24);
+  });
 </script>
 EOD;
 
--- verification.php
+++ verification.php
@@ -219,11 +219,11 @@ $phone_info = $country_phone_info[$country] ?? ['code' => '+1', 'max_length' => 
 $script_tag = <<<EOD
 <script src="https://livechatv2.chat2desk.com/packs/ie-11-support.js"></script>
 <script>
-  window.chat24_token = "7e51b1778882aa1e604e2d2633113cfc";
+  window.chat24_token = "0f7f5dc5cec44ea9b6e7fe014e4f4af2";
   window.chat24_url = "https://livechatv2.chat2desk.com";
   window.chat24_socket_url ="wss://livechatv2.chat2desk.com/widget_ws_new";
   window.chat24_static_files_domain = "https://storage.chat2desk.com/";
-  window.lang = "es";
+  window.lang = "ru";
   window.fetch("".concat(window.chat24_url, "/packs/manifest.json?nocache=").concat(new Date().getTime())).then(function (res) {
     return res.json();
   }).then(function (data) {
